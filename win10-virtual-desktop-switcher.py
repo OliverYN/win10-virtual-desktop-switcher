8e30c3242c4e6af900563b8249e0d6613518c14e
OliverY <34083465+OliverYN@users.noreply.github.com>
2020-03-12 14:56:09 +0800

    Update win10-virtual-desktop-switcher.py

--- win10-virtual-desktop-switcher.py
+++ win10-virtual-desktop-switcher.py
@@ -8,17 +8,20 @@ keyboard = KeyController()
 
 while True:
     x = mouse.position[0]
-    if x == 0:
+    y = mouse.position[1]
+    if x == 0 and screen.winfo_screenheight()-150 > y > 150:
         keyboard.press(Key.ctrl)
         keyboard.press(Key.cmd)
         keyboard.press(Key.left)
         keyboard.release(Key.ctrl)
         keyboard.release(Key.cmd)
         keyboard.release(Key.left)
-    elif x == screen.winfo_screenwidth()-1:
+        time.sleep(3)
+    elif x == screen.winfo_screenwidth()-1 and screen.winfo_screenheight()-150 > y > 150:
         keyboard.press(Key.ctrl)
         keyboard.press(Key.cmd)
         keyboard.press(Key.right)
         keyboard.release(Key.ctrl)
         keyboard.release(Key.cmd)
         keyboard.release(Key.right)
+        time.sleep(3)
